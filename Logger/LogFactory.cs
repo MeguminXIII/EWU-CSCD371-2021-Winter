3223b09872bceab42199b6f6c41b5b31195cb54b
Ryan Golden <rgolden5@eagles.ewu.edu>
2021-01-18 16:41:42 -0800

    LogFactory finished
    Hopefully finished LogFactory

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,11 +1,46 @@
-﻿namespace Logger
+﻿using System;
+using System.IO;
+
+namespace Logger
 {
     public class LogFactory
     {
-        
-    public BaseLogger CreateLogger(string className)
-        { 
-            return null!;
+        private string? _ClassName;
+        public string ClassName
+        {
+            get
+            {
+                return _ClassName!;
+            }
+            set => _ClassName = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public BaseLogger CreateLogger(string className)
+        {
+            BaseLogger? logger = null;
+            if (className == nameof(FileLogger))
+            {
+                ClassName = className;
+                string? filePath = ConfigureFileLogger();
+
+                while (filePath == null)
+                {
+                    Console.WriteLine("FilePath was null Please try again");
+                    filePath = Console.ReadLine();
+                }
+
+                logger = new FileLogger(filePath);
+
+            }
+                if (logger == null)
+                    throw new NullReferenceException("BaseLogger is null, try again");
+                return logger;
+        }
+
+        private string? ConfigureFileLogger()
+        {
+            Console.WriteLine("Please input a filepath");
+            return Console.ReadLine();
         }
     }
 }

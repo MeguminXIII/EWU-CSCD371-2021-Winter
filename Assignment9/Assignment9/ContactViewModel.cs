8cd32beb6cacaad0c46b19ec16697307a4d2d8de
Ryan Golden <rgolden5@eagles.ewu.edu>
2021-03-17 17:43:25 -0700

    Almost done, need to test and touch up

---
+++ Assignment9/Assignment9/ContactViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+    public class ContactViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string TwitterName { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DateTime _LastModifiedTime;
+
+        public DateTime LastModifiedTime
+        {
+            get => _LastModifiedTime;
+            set
+            {
+                _LastModifiedTime = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastModifiedTime)));
+            }
+        }
+    }
+}
---
+++ Assignment9/Assignment9/MainWindowViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Assignment9
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RelayCommand NewContactCmd { get; }
+        public RelayCommand DeleteContactCmd { get; }
+        public RelayCommand SaveContactCmd { get; }
+        public RelayCommand EditContactCmd { get; }
+        public ObservableCollection<ContactViewModel> ContactList { get; } = new();
+
+        private bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = "")
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+
+        private ContactViewModel _CurrentContact;
+        public ContactViewModel CurrentContact
+        {
+            get => _CurrentContact;
+            set => SetProperty(ref _CurrentContact, value);
+        }
+
+        private bool _InEdit;
+        public bool InEdit
+        {
+            get => _InEdit;
+            set => SetProperty(ref _InEdit, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            NewContactCmd = new RelayCommand(CreateNewContact, () => true);
+            DeleteContactCmd = new RelayCommand(DeleteContact, () => true);
+            SaveContactCmd = new RelayCommand(SaveContact, () => true);
+            EditContactCmd = new RelayCommand(EditContact, () => true);
+
+            ContactList.Add(new()
+            {
+                FirstName = "Inigo",
+                LastName = "Montoya",
+                PhoneNumber = "424-424-4242",
+                EmailAddress = "MontoyaI@email.com",
+                TwitterName = "@42%",
+                LastModifiedTime = DateTime.Now
+            });
+
+        }
+
+        private void EditContact()
+        {
+            InEdit = true;
+        }
+
+        private void SaveContact()
+        {
+            InEdit = false;
+            CurrentContact.LastModifiedTime = DateTime.Now;
+        }
+
+        private void DeleteContact()
+        {
+            ContactList.Remove(CurrentContact);
+        }
+
+        public bool CanDelete()
+        {
+            if (ContactList.Count > 0)
+                return true;
+            return false;
+        }
+
+        private void CreateNewContact()
+        {
+            ContactViewModel newContact = new ContactViewModel() { FirstName = "TO", LastName = "DO", LastModifiedTime = DateTime.Now };
+            ContactList.Add(newContact);
+            CurrentContact = newContact;
+            InEdit = true;
+        }
+    }
+
+}

00c0f2fdfb6ffe56cf9bb8b7fe3fd84d8ded143b
Ryan Golden <rgolden5@eagles.ewu.edu>
2021-03-18 16:27:03 -0700

    Added Some more tests

--- Assignment9/Assignment9.Tests/MainWindowViewModelTests.cs
+++ Assignment9/Assignment9.Tests/MainWindowViewModelTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Assignment9.Tests
 {
@@ -33,13 +34,13 @@ namespace Assignment9.Tests
         public void EditContact_ReturnsTrue()
         {
             MainWindowViewModel mvvm = new();
-
+            mvvm.InEdit = false;
             mvvm.EditContactCmd.Execute(null);
             Assert.IsTrue(mvvm.InEdit);
         }
 
         [TestMethod]
-        public void SaveContact_ReturnsFalse()
+        public void SaveContact_WithContactsInList_GoesFromInEditModeToViewMode()
         {
             MainWindowViewModel mvvm = new();
             mvvm.InEdit = true;
@@ -48,13 +49,37 @@ namespace Assignment9.Tests
         }
 
         [TestMethod]
-        public void SaveContact_NoContacts_NewContactIsGeneratedAndInEditForm()
+        public void SaveContact_NoContacts_NewContactIsGenerated()
         {
             MainWindowViewModel mvvm = new();
             mvvm.ContactList.Clear();
             Assert.AreEqual<int>(0, mvvm.ContactList.Count);
             mvvm.SaveContactCmd.Execute(null);
             Assert.AreEqual<int>(1, mvvm.ContactList.Count);
+        }
+
+        [TestMethod]
+        public void SaveContact_NoContactInList_PutsViewInEditForm()
+        {
+            MainWindowViewModel mvvm = new();
+            mvvm.ContactList.Clear();
+            mvvm.InEdit = false;
+            mvvm.SaveContactCmd.Execute(null);
+            Assert.IsTrue(mvvm.InEdit);
+        }
+
+        [TestMethod]
+        public void SaveContact_UpdatesLastModifiedTime()
+        {
+            MainWindowViewModel mvvm = new();
+
+            DateTime now = DateTime.Now;
+
+            mvvm.CurrentContact.LastModifiedTime = now;
+
+            mvvm.SaveContactCmd.Execute(null);
+
+            Assert.AreNotEqual<DateTime>(now, mvvm.CurrentContact.LastModifiedTime);
         }
     }
 }
---
+++ Assignment9/Assignment9.Tests/RelayCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9.Tests
+{
+    [TestClass]
+    public class RelayCommandTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RelayCommand_PassInNullValues_ThrowNullArgumentException()
+        {
+            RelayCommand relayCommand = new(null, null);
+        }
+    }
+}
